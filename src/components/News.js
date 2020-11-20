ae25956795b3a0331ad230e84087f7c3dfd812fe
ajjiitt <ajitdakre05@gmail.com>
2020-11-21 00:33:39 +0530

    first_save

---
+++ src/components/News.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { NewsContext } from "../NewsContext";
+import NewsArticle from "./NewsArticle";
+
+function News(props) {
+  const { data } = useContext(NewsContext);
+  console.log(data);
+
+  return (
+    <div>
+      <h1 className="head__text">!-!-!-!-!-MUMBAI TIMES-!-!-!-!-!</h1>
+      <hr/>
+      <div className="all__news">
+        {data
+          ? data.articles.map((news) => (
+              <NewsArticle data={news} key={news.url} />
+            ))
+          : "Loading"}
+      </div>
+    </div>
+  );
+}
+
+export default News;
